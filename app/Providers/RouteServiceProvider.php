6fe76e7d4cd69c3f83c0260eb1288c10b50cf953
TAstd22guA <FMLad_nov21z@outlook.jp>
2022-09-26 10:57:21 +0000

    toppage

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/login';
+    public const HOME = '/https://task-list-22set.herokuapp.com/login';
 
     /**
      * Define your route model bindings, pattern filters, etc.
